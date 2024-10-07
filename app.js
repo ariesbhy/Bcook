54d39d421fe981d883577cfb08b5b631a280600a
ariesbhy <basmahfarhan97@icloud.com>
2024-10-07 19:02:43 +0300

    create data

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express(); //instance of express
 
 //routes
-app.get("/categories", (req, res) => {});
-app.listen(8020, () => {
+app.get("/categories", (req, res) => {
+  console.log("categories");
+});
+
+app.listen(8025, () => {
   console.log("app is work on localhost 8020");
+  return res.json("categories");
 });
---
+++ data.js
@@ -0,0 +1,52 @@
+const categories = [
+  {
+    id: 1,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKxzYKq3A_FT-KGbDIy4bJk9d9PSozGPLWuQ&s",
+    name: "Soups & Salad",
+    descriotion:
+      " is made by combining ingredients of meat or vegetables with stock, milk, or water, vegetables ",
+  },
+  {
+    id: 2,
+    image:
+      "https://www.brit.co/media-library/image.jpg?id=21875108&width=600&quality=90",
+    name: "Appetizers",
+    description:
+      "appetizer is part of a meal that's served before the main course",
+  },
+  {
+    id: 3,
+    image:
+      "https://media.istockphoto.com/id/1403973419/photo/table-top-of-food-spread-on-table.jpg?s=612x612&w=0&k=20&c=2cROUMurZUtuvqF-bp8lViZ0wDXBNkZBcIjQj2QQlec=",
+    name: "Main Course",
+    description:
+      "main course represents the primary and typically the most substantial course of a meal",
+  },
+  {
+    id: 4,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg/1200px-%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg",
+    name: "Pasta",
+    description:
+      "Delicious pasta dishes from classic spaghetti Bolognese to lasagne and linguine",
+  },
+  {
+    id: 5,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Desserts.jpg/1200px-Desserts.jpg",
+    name: "Dessert",
+    description:
+      "Deserts are places where evaporation is more than the amount of rainfall",
+  },
+  {
+    id: 6,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7HM-KqdwpEjuJRi90OL335EyOpy66XCNgfA&s",
+    name: "Cold Drinks",
+    description:
+      "nonalcoholic beverages typically containing water, sweeteners, and some flavorings. Sometime, they are also called pops, soda, or fizzy drinks. They are called 'soft' in contrast to 'hard' drinks, which are generally referred to drinks with a significant alcohol content.",
+  },
+];
+
+export default categories;
