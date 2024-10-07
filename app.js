e57361dd24f580c74e4d94f2c8e3a75f120c94c9
ariesbhy <basmahfarhan97@icloud.com>
2024-10-07 18:57:51 +0300

    setup nodemone

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const app = express(); //instance of express
+
+//routes
+app.get("/categories", (req, res) => {});
+app.listen(8020, () => {
+  console.log("app is work on localhost 8020");
+});
